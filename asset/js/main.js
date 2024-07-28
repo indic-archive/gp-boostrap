51b6d0d2b8dc6611b8ed0efc94c168b9ce3789d4
Junaid PV <junu.pv@gmail.com>
2024-07-28 22:56:40 +0530

    Animate statistical numbers in the home page.

---
+++ asset/js/main.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+function animateValue(obj, start, end, duration) {
+  let startTimestamp = null;
+  const step = (timestamp) => {
+    if (!startTimestamp) startTimestamp = timestamp;
+    const progress = Math.min((timestamp - startTimestamp) / duration, 1);
+    obj.innerHTML = Math.floor(progress * (end - start) + start).toLocaleString();
+    if (progress < 1) {
+      window.requestAnimationFrame(step);
+    }
+  };
+  window.requestAnimationFrame(step);
+}
+
+(function($){
+  $( document ).ready(function() {
+    // Animate the numbers in home page.
+    $('.counts .theNum').each(function( index ) {
+      animateValue(this, 1, this.innerHTML, 1000);
+    });
+  });
+})(jQuery);
+  
+
