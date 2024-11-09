593bf4fd5c09d28af1bf713a8a7d065252a7b600
Junaid PV <junu.pv@gmail.com>
2024-11-09 10:31:51 +0530

    Use Indian locale

--- asset/js/main.js
+++ asset/js/main.js
@@ -4,7 +4,7 @@ function animateValue(obj, start, end, duration) {
   const step = (timestamp) => {
     if (!startTimestamp) startTimestamp = timestamp;
     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
-    obj.innerHTML = Math.floor(progress * (end - start) + start).toLocaleString();
+    obj.innerHTML = Math.floor(progress * (end - start) + start).toLocaleString('en-IN');
     if (progress < 1) {
       window.requestAnimationFrame(step);
     }
